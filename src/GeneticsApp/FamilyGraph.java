cb581d36209acd708fa9c95ac1a31b298ca8ab89
Jordan Nazemi <jnazemi@luc.edu>
2020-09-24 18:26:59 -0500

    Stuff

--- src/GeneticsApp/FamilyGraph.java
+++ src/GeneticsApp/FamilyGraph.java
@@ -16,7 +16,11 @@ class Person{
     String parents;
     String id;
 
-    public Person(String id)
+    public Person()
+    {
+        //TODO
+    }
+    public void setId(String id)
     {
         this.id = id;
     }
@@ -136,13 +140,13 @@ class RelationshipEdge
         extends
         DefaultEdge
 {
-    private String label;
-    public RelationshipEdge(String label)
+    private Relationship label;
+    public RelationshipEdge(Relationship label)
     {
         this.label = label;
     }
 
-    public String getLabel()
+    public Relationship getLabel()
     {
         return label;
     }
@@ -172,6 +176,15 @@ public class FamilyGraph {
 //        g.addVertex(tony);
 //
 //        g.addEdge(bob, tom, new RelationshipEdge("1"));
+        ArrayList<Hashtable> test2 = new ArrayList<Hashtable>();
+        Hashtable <String, String> testHash2 = new  Hashtable <String, String>();
+        testHash2.put("MaleParent", "P1");
+        testHash2.put("FemaleParent", "");
+        testHash2.put("Description", "NOTHING HAPPEN");
+        testHash2.put("EndDate", "NOW");
+        testHash2.put("StartDate", "LATER");
+        testHash2.put("Key", "R1");
+        test2.add(testHash2);
 //
 
 
@@ -180,35 +193,46 @@ public class FamilyGraph {
         testHash.put("GivenName", "Bob");
         testHash.put("FamilyName", "Fuchs");
         testHash.put("Key", "P1");
-        testHash.put("Suffix", "Jr");
+        testHash.put("Suffix", "Sr.");
         testHash.put("DOD", "yesterday");
         testHash.put("DOB", "today");
         testHash.put("BirthPlace", "St Louis");
         testHash.put("DeathPlace", "Chicago");
         testHash.put("Parents", "None");
-
         test.add(testHash);
-        DefaultUndirectedGraph<Person, RelationshipEdge> g = createGraph(test);
 
-        GraphIterator<Person, RelationshipEdge> iterator =
-                new BreadthFirstIterator<Person, RelationshipEdge>(g);
-        while (iterator.hasNext()) {
-            Person man = iterator.next();
-            System.out.println( man.getFirstName() );
-            System.out.println( man.getLastName() );
-            System.out.println( man.getSuffix() );
-            System.out.println( man.getDob() );
-            System.out.println( man.getDod() );
-        }
+        Hashtable <String, String> testHash3 = new  Hashtable <String, String>();
+        testHash3.put("GivenName", "Tony");
+        testHash3.put("FamilyName", "Fuchs");
+        testHash3.put("Key", "P2");
+        testHash3.put("Suffix", "Jr");
+        testHash3.put("DOD", "yesterday");
+        testHash3.put("DOB", "today");
+        testHash3.put("BirthPlace", "St Louis");
+        testHash3.put("DeathPlace", "Chicago");
+        testHash3.put("Parents", "R1");
+        test.add(testHash3);
 
-//        for(RelationshipEdge edge : g.edgeSet())
-//        {
-//            System.out.println(edge.getLabel());
+
+        DefaultUndirectedGraph<Person, RelationshipEdge> g = createGraph(test, test2);
+
+//        GraphIterator<Person, RelationshipEdge> iterator =
+//                new BreadthFirstIterator<Person, RelationshipEdge>(g);
+//        while (iterator.hasNext()) {
+//            Person man = iterator.next();
+//            System.out.println( man.getId() );
 //        }
+//
+        for(RelationshipEdge edge : g.edgeSet())
+        {
+            System.out.println(edge.getLabel().getId());
+//            System.out.println(edge.getLabel().getMaleParent().getId());
+//            System.out.println(edge.getLabel().getFemaleParent().getId());
+        }
 
     }
 
-    public static DefaultUndirectedGraph createGraph(ArrayList<Hashtable> people)
+    public static DefaultUndirectedGraph createGraph(ArrayList<Hashtable> people, ArrayList<Hashtable> relationships)
     {
         DefaultUndirectedGraph<Person, RelationshipEdge> g = new DefaultUndirectedGraph<>(RelationshipEdge.class);
 
@@ -217,7 +241,8 @@ public class FamilyGraph {
         {
             Hashtable personData = people.get(i);
             String id = (String) personData.get("Key");
-            Person person = new Person(id);
+            Person person = new Person();
+            person.setId(id);
 
             person.setFirstName((String) personData.get("GivenName"));
             person.setLastName((String) personData.get("FamilyName"));
@@ -227,9 +252,103 @@ public class FamilyGraph {
             person.setDod((String) personData.get("DOD"));
             person.setBirthPlace((String) personData.get("BirthPlace"));
             person.setDeathPlace((String) personData.get("DeathPlace"));
+            person.setParents((String)personData.get("Parents"));
 
             g.addVertex(person);
         }
+
+        for(i = 0; i < relationships.size(); i++)
+        {
+            Hashtable relationshipData = relationships.get(i);
+            String id = (String) relationshipData.get("Key");
+            Relationship r = new Relationship(id);
+
+            String maleID;
+            String femaleID;
+            if((String) relationshipData.get("MaleParent") == "")
+            {
+                maleID = "Unknown";
+            }
+            else
+            {
+                maleID = (String) relationshipData.get("MaleParent");
+            }
+
+            if((String) relationshipData.get("FemaleParent") == "")
+            {
+                femaleID = "Unknown";
+            }
+            else
+            {
+                femaleID = (String) relationshipData.get("FemaleParent");
+            }
+
+
+            Person male = new Person();
+            Person female = new Person();
+            GraphIterator<Person, RelationshipEdge> iterator = new BreadthFirstIterator<Person, RelationshipEdge>(g);
+
+            while (iterator.hasNext()) {
+                    Person person = iterator.next();
+                    if(person.getId() == maleID)
+                    {
+                        male = person;
+                    }
+                    else if (person.getId() == femaleID)
+                    {
+                        female = person;
+                    }
+                }
+            if(male.getId() == null)
+            {
+                male.setId("Unknown");
+                g.addVertex(male);
+            }
+
+            if (female.getId() == null)
+            {
+                female.setId("Unknown");
+                g.addVertex(female);
+            }
+
+            r.setMaleParent(male);
+            r.setFemaleParent(female);
+            r.setDescription((String) relationshipData.get("Description"));
+            r.setEndDate((String) relationshipData.get("EndDate"));
+            r.setStartDate((String) relationshipData.get("StartDate"));
+
+            RelationshipEdge edge = new RelationshipEdge(r);
+            g.addEdge(male, female, edge);
+        }
+
+
+        GraphIterator<Person, RelationshipEdge> iterator =
+                new BreadthFirstIterator<Person, RelationshipEdge>(g);
+        while (iterator.hasNext()) {
+            Person man = iterator.next();
+
+            Person maleParent = new Person();
+            Person femaleParent = new Person();
+            for(RelationshipEdge edge : g.edgeSet())
+            {
+//                System.out.println(edge.getLabel().getId());
+//                System.out.println(man.getParents());
+                if(edge.getLabel().getId() == man.getParents())
+                {
+                    maleParent = edge.getLabel().getMaleParent();
+                    femaleParent = edge.getLabel().getFemaleParent();
+
+                    Relationship maleRelationship = new Relationship(String.format("Child-%s",  man.getId()));
+                    RelationshipEdge maleEdge = new RelationshipEdge(maleRelationship);
+                    g.addEdge(man, maleParent, maleEdge);
+
+                    Relationship femaleRelationship = new Relationship(String.format("Child-%s", man.getId()));
+                    RelationshipEdge femaleEdge = new RelationshipEdge(femaleRelationship);
+                    g.addEdge(man, femaleParent, femaleEdge);
+                }
+            }
+        }
+
         return g;
     }
 }
---
+++ src/GeneticsApp/test.java
@@ -0,0 +1,12 @@
+package GeneticsApp;
+
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.MutableGraph;
+
+public class test {
+    public static void main(String[] args)
+    {
+        MutableGraph<String> guava = GraphBuilder.undirected().build();
+
+    }
+}
