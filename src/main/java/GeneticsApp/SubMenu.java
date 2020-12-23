edc2bf53ced28a52b160e66ba1cfb85556677b92
Collin Jones <cjones28@luc.edu>
2020-12-22 22:32:26 -0600

    front end window for add person

--- src/main/java/GeneticsApp/SubMenu.java
+++ src/main/java/GeneticsApp/SubMenu.java
@@ -16,12 +16,18 @@ import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 
-public class SubMenu {
+public class SubMenu { //TODO add a constructor for creating window (cut down on code) look at old projects for example
 
     Stage stage = new Stage();
     VBox branch = new VBox(10);
     TextField inputField = new TextField("field");
     TextField inputField2 = new TextField("field");
+    TextField inputField3 = new TextField("field");
+    TextField inputField4 = new TextField("field");
+    TextField inputField5 = new TextField("field");
+    TextField inputField6 = new TextField("field");
+    TextField inputField7 = new TextField("field");
+    TextField inputField8 = new TextField("field");
     Button closeButton = new Button("enter");
 
 
@@ -107,7 +113,36 @@ public class SubMenu {
     }
 
     public void displayAddPerson(Font font){
+        stage.setTitle("Explore Graph!");
+        stage.setHeight(900);
+        stage.setWidth(800);
+        stage.initModality(Modality.APPLICATION_MODAL);
 
+        inputField.setText("Type Unique Id (required)");
+        inputField2.setText("Enter first name");
+        inputField3.setText("Enter last name");
+        inputField4.setText("Enter suffix"); //TODO make this a drop down box like gender in dojo club
+        inputField5.setText("Enter a DOB (mm/dd/yyyy)"); //TODO make this a drop down to pick valid dates
+        inputField6.setText("Enter a birth place");
+        inputField7.setText("Enter a DOD (mm/dd/yyyy)");
+        inputField8.setText("Enter a death place");
+        SetStyle.setTextFieldStyle(font,inputField);
+        SetStyle.setTextFieldStyle(font,inputField2);
+        SetStyle.setTextFieldStyle(font,inputField3);
+        SetStyle.setTextFieldStyle(font,inputField4);
+        SetStyle.setTextFieldStyle(font,inputField5);
+        SetStyle.setTextFieldStyle(font,inputField6);
+        SetStyle.setTextFieldStyle(font,inputField7);
+        SetStyle.setTextFieldStyle(font,inputField8);
+
+        closeButton.setText("Press to add. Fields except for Id can be empty");
+        SetStyle.setButtonStyle(font,closeButton);
+        closeButton.setOnAction(e -> stage.close());
+
+        branch.getChildren().addAll(inputField,inputField2,inputField3,inputField4,inputField5,inputField6,inputField7,inputField8,closeButton);
+        Scene scene = new Scene(branch);
+        stage.setScene(scene);
+        stage.showAndWait();
     }
 
     //can probably put the import file method from familyGraph in here
